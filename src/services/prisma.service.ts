203decdd72ddd827d75a5ab88db15aff23bdc628
Clément <clementfiesta@gmail.com>
2024-12-21 13:46:59 +0100

    Add soft delete

--- src/services/prisma.service.ts
+++ src/services/prisma.service.ts
@@ -8,6 +8,24 @@ export class PrismaService
 {
   constructor() {
     super({ log: ['warn', 'error'] });
+
+    this.$use(async (params, next) => {
+      if (params.action === 'delete') {
+        // Change action to an update
+        params.action = 'update';
+        params.args.data = { deletedAt: new Date() };
+      }
+      if (params.action === 'deleteMany') {
+        // Change action to an updateMany
+        params.action = 'updateMany';
+        if (params.args.data !== undefined) {
+          params.args.data.deletedAt = new Date();
+        } else {
+          params.args.data = { deletedAt: new Date() };
+        }
+      }
+      return next(params);
+    });
   }
 
   async onModuleInit() {
