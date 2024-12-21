5bc6b86b4bccc7cdaa23c0424c7d53f392b60e7a
Clément <clementfiesta@gmail.com>
2024-12-21 19:09:11 +0100

    Move gameController into gamesController

--- src/controllers/games.controller.ts
+++ src/controllers/games.controller.ts
@@ -1,30 +1,76 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Optional,
+  Param,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Request, Response } from 'express';
 import { Ordering } from 'share-ur-save-common';
 import { GamesService } from 'src/services/games.service';
 
-@Controller('games')
-export class GamesController {
-  constructor(private gameService: GamesService) {}
+class GetGameDto {
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+}
 
-  @Get('/')
+class GetGamesDto {
+  @IsString()
+  @Optional()
+  keyword: string;
+
+  @IsNumber()
+  @Optional()
+  size: number;
+
+  @IsString()
+  @Optional()
+  sort: Ordering;
+}
+
+const BASE_URL = '/game';
+
+@Controller('')
+export class GamesController {
+  constructor(private gamesService: GamesService) {}
+
+  @Get(`${BASE_URL}/:id`)
+  async getGame(
+    @Param() params: GetGameDto,
+    @Req() request: Request,
+    @Res() response: Response,
+  ) {
+    console.log('[GAME CONTROLLER] Get game request');
+
+    if (!params.id) throw new BadRequestException('ID required');
+
+    const data = await this.gamesService.getGame(params.id);
+
+    return response.status(200).send({ game: data });
+  }
+
+  @Get(`/games`)
   async getGames(
-    @Query('keyword') keyword?: string,
-    @Query('size') size?: number,
-    @Query('sort') sort?: Ordering,
+    @Query() query: GetGamesDto,
+    @Req() request: Request,
+    @Res() response: Response,
   ) {
     console.log('[GAMES CONTROLLER] Get games request');
 
     const params: any = {};
 
-    if (size) params.size = size;
-    if (sort) params.sort = sort;
+    if (query.size) params.size = query.size;
+    if (query.sort) params.sort = query.sort;
 
-    const games = await this.gameService.getGames(keyword, params);
+    const games = await this.gamesService.getGames(query.keyword, params);
 
-    return {
-      keyword,
-      count: games.length,
-      games,
-    };
+    return response
+      .status(200)
+      .send({ keyword: query.keyword, count: games.length, games });
   }
 }
